748822ac2e4952d53582418316f2febcb6a1b96a
LeonAnthonyMenge <s0584337@htw-berlin.de>
2023-06-28 22:33:10 +0200

    Revert "try to fix href to NoteHome"
    This reverts commit 48f20360e341aa6e5d78fd15d64185a504af5a1b.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
-module.exports = {
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
-    port: '3000'
+    port: 3000,
   },
   publicPath: '/WebTech-Frontend/',
-};
+});
