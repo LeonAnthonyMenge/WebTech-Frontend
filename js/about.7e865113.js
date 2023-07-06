cc2da1e1ab8e2ae9df63c0aed92d326ddf3e929b
CI <CI@example.com>
2023-07-06 18:56:22 +0000

    deploy

---
+++ js/about.7e865113.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkWebTech_Frontend"]=self["webpackChunkWebTech_Frontend"]||[]).push([[443],{833:function(n,e,t){t.r(e),t.d(e,{default:function(){return b}});var r=t(3396);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),c=[a];function s(n,e){return(0,r.wg)(),(0,r.iD)("div",u,c)}var o=t(89);const i={},h=(0,o.Z)(i,[["render",s]]);var b=h}}]);
+//# sourceMappingURL=about.7e865113.js.map
